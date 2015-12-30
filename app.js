cbd02ad38cef6723173894736bba894daae8dee8
bikerp <gimragit1>
2015-12-30 19:28:52 +0100

    Initial import

---
+++ app.js
@@ -0,0 +1,39 @@
+/**
+ * Tool for reading data from D-Link DSP-W215 Home Smart Plug.
+ *
+ * Usage: enter your PIN code to LOGIN_PWD, change value of HNAP_URL according to your device settings.
+ *
+ * @type {exports|module.exports}
+ */
+var soapclient = require('./js/soapclient');
+var fs = require('fs');
+
+var OUTPUT_FILE = "result.txt";
+var LOGIN_USER = "admin";
+var LOGIN_PWD = "<PIN CODE>";
+var HNAP_URL = "http://192.168.1.128/HNAP1";
+var POLLING_INTERVAL = 10000;
+
+var login = soapclient.login(LOGIN_USER, LOGIN_PWD, HNAP_URL);
+
+login.done(function () {
+    read();
+});
+
+function read() {
+    soapclient.sendCommand("GetCurrentPowerConsumption", "CurrentConsumption", 1).done(function (power) {
+        soapclient.sendCommand("GetCurrentTemperature", "CurrentTemperature", 2).done(function (temperature) {
+            console.log(new Date().toLocaleString(), power, temperature);
+            save(power,temperature);
+            setTimeout(function () {
+                read();
+            }, POLLING_INTERVAL);
+        });
+    })
+}
+
+function save(power, temperature){
+    fs.writeFile(OUTPUT_FILE, new Date().toLocaleString()+";"+power+";"+temperature+"\r\n",{flag:"a"}, function(err){
+        if(err) throw err;
+    })
+}
---
+++ js/soapclient.js
@@ -0,0 +1,120 @@
+var md5 = require('./hmac_md5');
+var request = require('then-request');
+var DOMParser = require('xmldom').DOMParser;
+var fs = require("fs");
+
+var HNAP1_XMLNS = "http://purenetworks.com/HNAP1/";
+var HNAP_METHOD = "POST";
+var HNAP_BODY_ENCODING = "UTF8";
+var HNAP_LOGIN_METHOD = "Login";
+
+var HNAP_AUTH = {URL:"", User: "", Pwd: "", Result: "", Challenge: "", PublicKey: "", Cookie: "", PrivateKey: ""};
+
+exports.login = function (user, password, url) {
+    HNAP_AUTH.User = user;
+    HNAP_AUTH.Pwd = password;
+    HNAP_AUTH.URL = url;
+
+    return request(HNAP_METHOD, HNAP_AUTH.URL,
+        {
+            headers: {
+                "Content-Type": "text/xml; charset=utf-8",
+                "SOAPAction": '"' + HNAP1_XMLNS + HNAP_LOGIN_METHOD + '"'
+            },
+            body: requestBody(HNAP_LOGIN_METHOD, loginRequest())
+        }).then(function (response) {
+                save_login_result(response.getBody(HNAP_BODY_ENCODING));
+                return processLogin(HNAP_LOGIN_METHOD);
+    });
+};
+
+function processLogin(method) {
+    return request(HNAP_METHOD, HNAP_AUTH.URL,
+        {
+        headers: {
+            "Content-Type": "text/xml; charset=utf-8",
+            "SOAPAction": '"' + HNAP1_XMLNS + method + '"',
+            "HNAP_AUTH": getHnapAuth('"' + HNAP1_XMLNS + method + '"', HNAP_AUTH.PrivateKey),
+            "Cookie": "uid=" + HNAP_AUTH.Cookie
+        },
+        body: requestBody(method, loginParameters())
+    }).then(function (response) {
+        return;
+    });
+}
+function save_login_result(body) {
+    var doc = new DOMParser().parseFromString(body);
+    HNAP_AUTH.Result = doc.getElementsByTagName(HNAP_LOGIN_METHOD + "Result").item(0).firstChild.nodeValue;
+    HNAP_AUTH.Challenge = doc.getElementsByTagName("Challenge").item(0).firstChild.nodeValue;
+    HNAP_AUTH.PublicKey = doc.getElementsByTagName("PublicKey").item(0).firstChild.nodeValue;
+    HNAP_AUTH.Cookie = doc.getElementsByTagName("Cookie").item(0).firstChild.nodeValue;
+    HNAP_AUTH.PrivateKey = md5.hex_hmac_md5(HNAP_AUTH.PublicKey + HNAP_AUTH.Pwd, HNAP_AUTH.Challenge).toUpperCase();
+}
+
+function requestBody(method, parameters) {
+    var str = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+        "<soap:Envelope " +
+        "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+        "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
+        "xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+        "<soap:Body>" +
+        "<" + method + " xmlns=\"";
+
+    str += HNAP1_XMLNS;
+
+    str += "\">" +
+        parameters +
+        "</" + method + "></soap:Body></soap:Envelope>";
+    return str;
+}
+
+function commandParameters(module) {
+    var param = "<ModuleID>" + module + "</ModuleID>";
+    return param;
+}
+
+exports.sendCommand = function (method, responseElement, module) {
+    return request(HNAP_METHOD, HNAP_AUTH.URL,
+        {
+        headers: {
+            "Content-Type": "text/xml; charset=utf-8",
+            "SOAPAction": '"' + HNAP1_XMLNS + method + '"',
+            "HNAP_AUTH": getHnapAuth('"' + HNAP1_XMLNS + method + '"', HNAP_AUTH.PrivateKey),
+            "Cookie": "uid=" + HNAP_AUTH.Cookie
+        },
+        body: requestBody(method, commandParameters(module))
+    }).then(function (response) {
+        return readResponseValue(response.getBody(HNAP_BODY_ENCODING), responseElement)
+    });
+};
+
+function loginRequest() {
+    var para = "<Action>request</Action>"
+        + "<Username>" + HNAP_AUTH.User + "</Username>"
+        + "<LoginPassword></LoginPassword>"
+        + "<Captcha></Captcha>";
+
+    return para;
+}
+
+function loginParameters() {
+    var login_pwd = md5.hex_hmac_md5(HNAP_AUTH.PrivateKey, HNAP_AUTH.Challenge);
+    var para = "<Action>login</Action>"
+        + "<Username>" + HNAP_AUTH.User + "</Username>"
+        + "<LoginPassword>" + login_pwd.toUpperCase() + "</LoginPassword>"
+        + "<Captcha></Captcha>";
+    return para;
+}
+
+function getHnapAuth(SoapAction, privateKey) {
+    var current_time = new Date();
+    var time_stamp = Math.round(current_time.getTime() / 1000);
+    var auth = md5.hex_hmac_md5(privateKey, time_stamp + SoapAction);
+    return auth.toUpperCase() + " " + time_stamp;
+}
+
+function readResponseValue(body, elementName) {
+    var doc = new DOMParser().parseFromString(body);
+    var node = doc.getElementsByTagName(elementName).item(0);
+    return (node) ? node.firstChild.nodeValue : "ERROR";
+}
